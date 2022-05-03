addff75226d05de37318b7d94c632ca9ce55fda0
Brandon Heine <brandonmheine@gmail.com>
2022-05-03 15:41:53 -0700

    Working on getting goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const {
-  getGoals, 
-  setGoal, 
-  updateGoal, 
+  getGoals,
+  setGoal,
+  updateGoal,
   deleteGoal,
 } = require('../controllers/goalController')
 
-const {protect} = require('../middleware/authMiddleWare')
+const { protect } = require('../middleware/authMiddleware')
 
-// These two lines are the same as the 4 lines below
 router.route('/').get(protect, getGoals).post(protect, setGoal)
-router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
-
-// router.get('/', getGoals)
-// router.post('/', setGoal)
-// router.put('/:id', updateGoal)
-// router.delete('/:id', deleteGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 module.exports = router
