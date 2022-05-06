3b5a0f1980da98bf36d8d78e3bf97cbcc4b1449d
Brandon Heine <brandonmheine@gmail.com>
2022-05-06 14:34:56 -0700

    Login, logout, register are working

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import App from "./App.js"
+import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import "./index.css"
 
@@ -22,3 +22,21 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import { store } from './app/store';
+// import { Provider } from 'react-redux';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
